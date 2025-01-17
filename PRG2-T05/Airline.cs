7fb56b926178484263db94f528b1475bf1f5c7e1
Yeaw Min Lee <yeawminlee@gmail.com>
2025-01-17 15:30:15 +0800

    Committed Airline and BoardingGate as well Terminal requesting Quality Control from Owner.

--- PRG2-T05/Airline.cs
+++ PRG2-T05/Airline.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿// Airline.cs
+using PRG2_T05_Flight;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace PRG2_T05
+public class Airline
 {
-    internal class Airline
+    public string Name { get; set; }
+    public string Code { get; set; }
+    public Dictionary<string, Flight> Flights { get; private set; } = new Dictionary<string, Flight>();
+
+    public Airline(string name, string code)
     {
+        Name = name;
+        Code = code;
+    }
+
+    public bool AddFlight(Flight flight)
+    {
+        if (!Flights.ContainsKey(flight.FlightNumber))
+        {
+            Flights[flight.FlightNumber] = flight;
+            return true;
+        }
+        return false;
+    }
+
+    public bool RemoveFlight(Flight flight)
+    {
+        return Flights.Remove(flight.FlightNumber);
+    }
+
+    public double CalculateFees()
+    {
+        double totalFees = 0;
+        foreach (var flight in Flights.Values)
+        {
+            totalFees += flight.CalculateFees();
+        }
+        return totalFees;
+    }
+
+    public override string ToString()
+    {
+        return $"Airline: {Name} ({Code}), Total Flights: {Flights.Count}";
     }
 }
--- PRG2-T05/Terminal.cs
+++ PRG2-T05/Terminal.cs
@@ -1,12 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Terminal.cs
+using PRG2_T05_Flight;
 
-namespace PRG2_T05
+public class Terminal
 {
-    internal class Terminal
+    public string TerminalName { get; set; }
+    public Dictionary<string, Airline> Airlines { get; private set; } = new Dictionary<string, Airline>();
+    public Dictionary<string, BoardingGate> BoardingGates { get; private set; } = new Dictionary<string, BoardingGate>();
+
+    public Terminal(string terminalName)
     {
+        TerminalName = terminalName;
+    }
+
+    public bool AddAirline(Airline airline)
+    {
+        if (!Airlines.ContainsKey(airline.Code))
+        {
+            Airlines[airline.Code] = airline;
+            return true;
+        }
+        return false;
+    }
+
+    public bool AddBoardingGate(BoardingGate gate)
+    {
+        if (!BoardingGates.ContainsKey(gate.GateName))
+        {
+            BoardingGates[gate.GateName] = gate;
+            return true;
+        }
+        return false;
+    }
+
+    public Airline GetAirlineFromFlight(Flight flight)
+    {
+        foreach (var airline in Airlines.Values)
+        {
+            if (airline.Flights.ContainsKey(flight.FlightNumber))
+            {
+                return airline;
+            }
+        }
+        return null;
+    }
+
+    public void PrintAirlineFees()
+    {
+        foreach (var airline in Airlines.Values)
+        {
+            Console.WriteLine($"{airline.Name} Fees: ${airline.CalculateFees():F2}");
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"Terminal: {TerminalName}, Airlines: {Airlines.Count}, Gates: {BoardingGates.Count}";
     }
 }
